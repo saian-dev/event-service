c9077865c8d4cd18c1954e6711d3df4941d7cc2f
Saian <sayan.badarchy@gmail.com>
2025-04-09 21:55:10 +0400

    remove hardcoded run command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from contextlib import asynccontextmanager
 
-import uvicorn
 from app.api.routers import router_v1
 from app.core.database import sessionmanager
 from fastapi import FastAPI
@@ -16,7 +15,3 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan, docs_url="/api/docs")
 app.include_router(router_v1, prefix="/api")
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
